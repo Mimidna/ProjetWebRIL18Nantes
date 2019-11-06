4c3c131459ad6ecc4020deb996b4d837faf2946d
Loren JOUAN <loren.jouan@sogeti.com>
2019-11-06 09:47:52 +0100

    Header Ok LJ

---
+++ include/header.php
@@ -0,0 +1,19 @@
+<head>
+    <!-- META INFORMATIONS-->
+    <title>Voltik</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+    <meta name="description" content="Voltik">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!--BOOSTRAP-->
+
+    <link rel="stylesheet" type="text/css" href="css/bootstrap4.3.1.css" />
+
+    <!--CSS-->
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+
+
+    
+
+</head>
