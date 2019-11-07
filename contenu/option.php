98f478b2dd38dc7a434de00e3d00df200d93c118
Mimidna <53787715+Mimidna@users.noreply.github.com>
2019-11-07 13:15:29 +0100

    Maj david option.php

---
+++ contenu/option.php
@@ -0,0 +1,79 @@
+<h1>Options</h1>
+<br/>
+<form>
+    <div class="champCheckbox">
+        <legend>Couleurs</legend>
+                </br>
+                    <input type="checkbox" id="Noir" name="Noir">
+                    <label for="Noir">Noir</label>
+                </br>
+                    <input type="checkbox" id="Bleu" name="Bleu">
+                    <label for="Bleu">Bleu Nuit</label>
+                </br>
+                    <input type="checkbox" id="Blanc" name="Blanc">
+                    <label for="Blanc">Blanc Gris</label>
+                </br>
+                    <input type="checkbox" id="Bordeaux" name="Bordeaux">
+                    <label for="Bordeaux">Bordeaux</label>
+    </div>
+    </br>
+    <div class="champCheckbox">
+        <legend>Couleurs de retroviseurs</legend>
+                </br>
+                    <input type="checkbox" id="deBase" name="deBase">
+                    <label for="deBase">De base</label>
+                </br>
+                    <input type="checkbox" id="Noir" name="Noir">
+                    <label for="Noir">Noir</label>
+                </br>
+                    <input type="checkbox" id="Bleu" name="Bleu">
+                    <label for="Bleu">Bleu Nuit</label>
+                </br>
+                    <input type="checkbox" id="Blanc" name="Blanc">
+                    <label for="Blanc">Blanc Gris</label>
+                </br>
+                    <input type="checkbox" id="Bordeaux" name="Bordeaux">
+                    <label for="Bordeaux">Bordeaux</label>
+    </div>
+    </br>
+    <div class="champCheckbox">
+        <legend>GPS</legend>
+                </br>
+                    <input type="checkbox" id="Avec" name="Avec">
+                    <label for="Avec">Avec</label>
+                </br>
+                    <input type="checkbox" id="Sans" name="Sans">
+                    <label for="Sans">Sans</label>
+    </div>
+    </br>
+    <div class="champCheckbox">
+        <legend>Toit</legend>
+                </br>
+                    <input type="checkbox" id="CLassique" name="Classique">
+                    <label for="Classique">Classique</label>
+                </br>
+                    <input type="checkbox" id="Panoramique" name="Panoramique">
+                    <label for="Panoramique">Panoramique</label>
+                </br>
+                    <input type="checkbox" id="Ouvrant" name="Ouvrant">
+                    <label for="Ouvrant">Ouvrant</label>
+    </div>
+    </br>
+    <div class="champCheckbox">
+        <legend>Camara d'aide au stationnement</legend>
+                    </br>
+                        <input type="checkbox" id="Sans" name="Sans">
+                        <label for="Sans">Sans</label>
+                    </br>
+                        <input type="checkbox" id="Arriere" name="Arriere">
+                        <label for="Arriere">Arriere</label>
+                    </br>
+                        <input type="checkbox" id="AvantetArriere" name="AvantetArriere">
+                        <label for="AvantetArriere">Avant et Arriere</label>
+    </div>
+    </br>
+    <div class ="boutonValider">
+        <input type="submit" value="Valider les options">
+    </div>
+    
+</form>
