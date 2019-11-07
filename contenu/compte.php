5c10b41bd64da3b1cb483edbd7a738015f5e10bd
Mimidna <53787715+Mimidna@users.noreply.github.com>
2019-11-07 11:51:26 +0100

    Maj compte utilisateur

---
+++ contenu/compte.php
@@ -0,0 +1,61 @@
+ <!-- Page Content -->
+    <div class="container compte-page">
+
+		<ul class="nav flex-column">
+		<li class="nav-item fleche">
+		<a href="javascript:history.go(-1)"> Retour <img class="fleche-Image" alt="flechest" src="./css/logos/fb.png" /> </a> 
+		</li>
+		</ul>
+
+        <!-- Page titre -->
+        <div class="compte">
+            <strong>Compte </strong>
+
+        </div>
+
+        <!-- Contact Form -->
+
+        <div class="jumbotron Compte-client">
+            <div class="row">
+                <div class="col-xs-12 col-ms-12 col-lg-6 mb-6">
+                    
+                   
+                   
+                        <div  class="name">
+                           <strong> NOM : </strong> BLA
+                        </div>
+
+						<div class="prénom">
+						 <strong> PRENOM : </strong> Bla
+						</div>
+						<div class="téléphone">
+						 <strong> TELEPHONE : </strong> 01 01 01 01 01
+						</div>
+						<address>
+						<strong> ADRESSE :</strong>
+						<br> 12 rue de la rue<br>
+						NANTES,44000<br>
+						FRANCE
+						</address>
+						<div class="compte-modifier">
+						<a href="contact"> <img class="fleche" alt="flechest" src="./css/logos/fb.png" /></a>
+						
+											   						                      
+                </div>
+
+            </div>
+            <!-- /.row -->
+        </div>
+
+		<ul class="nav flex-column Compte-listeVoitures">
+		 <li class="nav-item ">
+			<a href="deviscorrespondant">   <strong> Voiture 1 : </strong> </br>
+			A1 créative 14 000 euro</a>
+		 </li>
+		 <li class="nav-item ">
+			<a href="deviscorrespondant"> <strong> Voiture 2 : </strong> </br>
+			A1 exclusive 17 000 euro</a>
+		 </li>
+		</ul>
+    </div>
+    <!-- /.container -->
