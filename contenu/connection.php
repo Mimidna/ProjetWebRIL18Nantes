07064643e402b02de84ace87ba9f4e79b7b53df9
Mimidna <53787715+Mimidna@users.noreply.github.com>
2019-11-07 13:16:48 +0100

    connexion.php maj david

---
+++ contenu/connection.php
@@ -0,0 +1,20 @@
+<h1>Connexion</h1>
+        <br/>
+        <form class="formulaireConnexion">
+            <div>
+                    <label><b>Nom d'utilisateur</b></label>
+                    <input type="text" name="nomUser">
+            </div>
+            <br/>
+            <div>
+                    <label><b>Mot de passe</b></label>
+                    <input type="password" name="pwdUser">
+            </div>
+            <br/>
+            <input type="submit" value="Se connecter">
+        </form>
+
+
+
+
+		<!--FOOTER-->
