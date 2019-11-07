7b5039805fe807414fcb2e0c00d9490e2a74afa3
NMARDE <49159478+NMARDE@users.noreply.github.com>
2019-11-07 11:43:24 +0100

    maj navbar

--- index.php
+++ index.php
@@ -12,47 +12,44 @@ include "include/header.php";
 
 <body >
 
+<!--ENTETE-->
+
+
+
 
 <!--NAVBAR-->
 
 <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12">
-			<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
+			<nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top bg-nico">
 
-
+			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+			<span class="navbar-toggler-icon"></span>
+			</button>
 				<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
 					<ul class="navbar-nav">
 						<li class="nav-item active">
-							 <a class="nav-link" href="#">ACCUEIL<span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item">
-							 <a class="nav-link" href="#">Link</a>
+							 <a class="nav-link" href="#">Accueil<span class="sr-only">(current)</span></a>
 						</li>
 						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
+							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Nos modèles</a>
 							<div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
+								 <a class="dropdown-item" href="#"></a> <a class="dropdown-item" href="#">Mini</a> <a class="dropdown-item" href="#">Berline</a> <a class="dropdown-item" href="#">Sportives</a> <a class="dropdown-item" href="#">SUV</a>
 							</div>
 						</li>
+						<li class="nav-item active">
+							 <a class="nav-link" href="#">Configurer</a>
+						</li>
 					</ul>
-					<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
-					<span class="navbar-toggler-icon"></span>
-				</button> <a class="navbar-brand" href="#">
+					<a class="navbar-brand" href="#">
 					<img src="css/logos/logo.png" alt>
 				</a>
 					<ul class="navbar-nav ml-md-auto">
-						<li class="nav-item active">
-							 <a class="nav-link" href="#">Link <span class="sr-only">(current)</span></a>
-						</li>
-						<li class="nav-item dropdown">
-							 <a class="nav-link dropdown-toggle" href="http://example.com" id="navbarDropdownMenuLink" data-toggle="dropdown">Dropdown link</a>
-							<div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdownMenuLink">
-								 <a class="dropdown-item" href="#">Action</a> <a class="dropdown-item" href="#">Another action</a> <a class="dropdown-item" href="#">Something else here</a>
-								<div class="dropdown-divider">
-								</div> <a class="dropdown-item" href="#">Separated link</a>
+						<button class="btn btn-primary my-2 my-sm-0 btn-nico" href="contenu/connection.php">
+							Se connecter
+						</button>
 							</div>
 						</li>
 					</ul>
@@ -64,9 +61,9 @@ include "include/header.php";
 
 
 <!--CONTENU-->
- 
+
  <?php
-	include "contenu/accueil.php";
+	include "contenu/acceuil.php";
 	?>
 
 
